68ae122e9573f31698d50f2dbd3fd2ef906eb02d
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-05-20 13:21:01 +0800

    mutual information

---
+++ src/mutual_info.py
@@ -0,0 +1,62 @@
+import numpy as np
+from typing import Union
+from sklearn.metrics import mutual_info_score
+
+from utils.basis_get import basis_get
+
+
+def mutual_info(
+    dataX: Union[str, bytes, list, tuple],
+    dataY: Union[str, bytes, list, tuple],
+    base=2,
+    basis_dict: Union[set, list, tuple, None] = None,
+):
+    """
+    Return the mutual information of given dataX and dataY.
+
+    Parameters
+    ----------
+    dataX:   str, bytes, or iterable
+    dataY:   str, bytes, or iterable
+    base:   int
+            The logarithmic base to use, default is 2.
+    basis_dict: list
+                The basis of the information, default is None. If None, the basis will be the unique elements in data.
+
+    Returns
+    -------
+    mi:    float
+            The mutual information of given dataX and dataY.
+
+    """
+
+    x = []
+    if type(dataX) is bytes:
+        x = list(dataX)
+    elif type(dataX) is str:
+        for char in dataX:
+            x.append(char)
+    else:
+        x = dataX
+
+    y = []
+    if type(dataY) is bytes:
+        y = list(dataY)
+    elif type(dataY) is str:
+        for char in dataY:
+            y.append(char)
+    else:
+        y = dataY
+
+    if len(x) != len(y):
+        print("The length of dataX and dataY should be equal.")
+        return None
+
+    mi = mutual_info_score(x, y)
+    return mi
+
+
+if __name__ == "__main__":
+    dataX = b"hello world!"
+    dataY = b"wheel rolled"
+    print(mutual_info(dataX, dataY))
